b6c07c79dd556cebc43497cdfe72b1b36bbd4978
Nelson Rodriguez <Nelson.Rodriguez@bitsamericas.com>
2022-03-09 20:58:50 -0500

    add crear_proyecto.php

---
+++ drupal_modules_udemy/crear_proyecto.php
@@ -0,0 +1,23 @@
+/modulos crear carpeta custom/<nombreCarpeta>/
+    dentro crear archivos
+    <nombreArchivo.info.yml>
+    <nombreArchivo.module>
+
+eje curso udemy
+nombreCarpeta = alterar_formulario
+nombreArchivo = alterar_formulario.info.yml
+nombreArchivo = alterar_fomulario.module
+
+agregar contenido en los dos archivos de acuerdo a dcumentacion
+ 
+luego extend buscar modulo e instalar
+
+ruta para buscar resultado eje
++field_celular
+    +widget
+        +0
+            +value
+            placeholder => copiar ruta  
+
+            ir archivo. module
+            $form[pegarRutaPlaceholder]="Digite su celular"
